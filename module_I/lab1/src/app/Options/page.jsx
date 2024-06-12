96d785c5ff7797354daf07d9d9b042782675e5bf
andersonUdec <uranopvp@gmail.com>
2024-06-11 21:44:09 -0500

    I changed all the html code from page options  for bootstrap react components

--- module_I/lab1/src/app/Options/page.jsx
+++ module_I/lab1/src/app/Options/page.jsx
@@ -1,21 +1,27 @@
 import Link from 'next/link';
 import styles from "./page.module.css";
+import { Container, Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 //Modulo de opciones para el inicio de sesión o registro de usuarios
-export default function OptionsPage (){
-    return ( 
-        <div className={styles.container}>
-            <h1>Hola.</h1>
-            <h1>Si no tienes una cuenta puedes registrarte.</h1>
-            <h1>Si ya cuentas con una puedes iniciar sesión.</h1>
-            <div className={styles.marginBottom}>
+export default function OptionsPage() {
+    return (
+        <Container className="d-flex flex-column align-items-center">
+            <Form className="text-center">
+                <h1>Hola.</h1>
+                <h1>Si no tienes una cuenta puedes registrarte.</h1>
+                <h1>Si ya cuentas con una puedes iniciar sesión.</h1>
+                <br />
                 <Link href="/Signup">
-                <button className={styles.buttonOptions}>Registrarse</button>
+                    <Button>Registrarse</Button>
                 </Link>
+                <br />
+                <br />
                 <Link href="/Signin">
-                <button className={styles.buttonOptions}>Iniciar Sesión</button>
+                    <Button>Iniciar Sesión</Button>
                 </Link>
-            </div>
-        </div>
-  );
+            </Form>
+
+        </Container>
+    );
 }
