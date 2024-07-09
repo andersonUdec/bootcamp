7b9136798c2a4000b40be5fda965ef27c5459746
andersonUdec <uranopvp@gmail.com>
2024-07-08 21:00:59 -0500

    Lab1 countGender and file elements.json

---
+++ module_IV/lab1/lab1.js
@@ -0,0 +1,23 @@
+const performance = require("perf_hooks");
+
+const elementsJson =  require('../elements.json');
+
+function countGenders(elements) {
+    const genderCounts = elements.reduce((genderName, element) => {
+        const gender = element.gender.toLowerCase().replace('-','');
+        if (genderName[gender]) {
+            genderName[gender]++;
+        } else {
+            genderName[gender] = 1;
+        }
+        return genderName;
+    }, {});
+
+    return genderCounts;
+}
+let start = performance.performance.now();
+const result = countGenders(elementsJson);
+let end = performance.performance.now();
+let durationInTime = end - start;
+console.log(`Time ${durationInTime}ms.`);
+console.log(result);
